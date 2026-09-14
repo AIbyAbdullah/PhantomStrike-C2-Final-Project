14c244012d91e109aa8a2dd811826f526f695c7c
AIbyAbdullah <hafizabdullahkhanak@gmail.com>
2026-09-14 12:28:09 +0500

    Change email credentials in email_alert.py
    Updated email credentials for sender and receiver.

--- email_alert.py
+++ email_alert.py
@@ -5,9 +5,9 @@ from email.mime.base import MIMEBase
 from email import encoders
 import os
 
-SENDER_EMAIL = "hafizabdullahkhanas@gmail.com"
-SENDER_PASSWORD = "hrcj nyjw vfjf ursv"  # 🔥 New App Password
-RECEIVER_EMAIL = "hafizabdullahkhanas@gmail.com"
+SENDER_EMAIL = "email"
+SENDER_PASSWORD = "emailcode"  # 🔥 New App Password
+RECEIVER_EMAIL = "email"
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 
